efbd3d63bb4326b77c791a8ad17bef7e550bcbbb
Aaron Parandian <aparandian@apple.com>
2025-11-29 16:41:07 -0800

    02. maps copy and clone ops

--- 02/main.go
+++ 02/main.go
@@ -16,6 +16,8 @@ func main() {
 	fmt.Println("Original cart: ", cart)
 	printCart(cart)
 
+	fmt.Println("----------------")
+
 	// key exists in map
 	if quantity, ok := cart["apple"]; ok {
 		fmt.Println("Apple quantity: ", quantity)
@@ -28,27 +30,43 @@ func main() {
 		fmt.Println("Grapes not found in cart")
 	}
 
+	fmt.Println("----------------")
+
 	// add key to map
 	cart["grapes"] = 4
 	fmt.Println("Updated cart: ", cart)
 	printCart(cart)
+
+	fmt.Println("----------------")
 
 	// remove key from map
 	delete(cart, "orange")
 	fmt.Println("Updated cart: ", cart)
 	printCart(cart)
 
+	fmt.Println("----------------")
+
 	clonedCart := maps.Clone(cart)
 	fmt.Println("Cloned cart: ", clonedCart)
 	printCart(clonedCart)
 
+	fmt.Println("----------------")
+
+	copiedCart := cart
+
 	// demonstrate maps are independent
 	cart["banana"] = 10
 	fmt.Println("Updated cart: ", cart)
+	fmt.Println("Copied cart: ", copiedCart)
 	printCart(cart)
+	printCart(copiedCart)
+
+	fmt.Println("----------------")
+
 	fmt.Println("Cloned cart: ", clonedCart)
 	printCart(clonedCart)
 
+	fmt.Println("----------------")
 }
 
 func printCart(cart map[string]int) {
